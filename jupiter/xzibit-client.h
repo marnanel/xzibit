6ce92d0d4632532341734930c0935757bf467215
Thomas Thurman <thomas.thurman@collabora.co.uk>
2010-08-15 10:12:08 -0400

    doxygen comments

--- jupiter/xzibit-client.h
+++ jupiter/xzibit-client.h
@@ -32,26 +32,107 @@
 
 typedef struct _XzibitClient XzibitClient;
 
+/**
+ * Creates an xzibit client connected to the
+ * xzibit server currently running on localhost.
+ *
+ * \return  The new client.
+ */
 XzibitClient* xzibit_client_new (void);
+
+/**
+ * Destroys an xzibit client and frees the
+ * memory.
+ *
+ * \param client  The client.
+ */
 void xzibit_client_free (XzibitClient *client);
+
+/**
+ * Sends an avatar to the xzibit server.
+ *
+ * \param client  The client.
+ * \param avatar  The image to use.
+ */
 void xzibit_client_send_avatar (XzibitClient *client,
                                 GdkPixbuf *avatar);
+
+/**
+ * Creates a video channel on an xzibit client.
+ *
+ * \param client  The client.
+ * \return  The channel ID.
+ */
 int xzibit_client_open_channel (XzibitClient *client);
+
+/**
+ * Closes a video channel on an xzibit client.
+ *
+ * \bug Should close associated audio channels
+ *      and doesn't.
+ *
+ * \param client  The client.
+ * \param channel The channel ID.
+ */
 void xzibit_client_close_channel (XzibitClient *client,
                                   int channel);
+
+/**
+ * Sets the window title for a channel.
+ *
+ * \param client  The client.
+ * \param channel The channel ID.
+ * \param title   The new title, UTF-8 encoded.
+ */
 void xzibit_client_set_title (XzibitClient *client,
                               int channel,
                               char *title);
+/**
+ * Sets the window icon for a channel.  The
+ * image will be scaled on the client side as
+ * appropriate.
+ *
+ * \param client  The client.
+ * \param channel The channel ID.
+ * \param icon    The icon to use.
+ */
 void xzibit_client_set_icon (XzibitClient *client,
                              int channel,
                              GdkPixbuf *icon);
+
+/**
+ * Sends a still image (and may be misnamed).
+ *
+ * \param client  The client.
+ * \param channel The channel ID.
+ * \param image   The image to send.
+ */
 void xzibit_client_send_video (XzibitClient *client,
                                int channel,
                                GdkPixbuf *image);
+/**
+ * Sends audio.
+ *
+ * \param client  The client.
+ * \param channel The channel ID of the video channel.
+ *                Don't bother working out audio channel
+ *                IDs; the client does that for you.
+ * \param wave    16-bit PCM data at 44100 Hz.
+ * \param length  Length of "wave", in bytes.
+ */
 void xzibit_client_send_audio (XzibitClient *client,
                                int channel,
                                gpointer wave,
                                gsize length);
+
+/**
+ * Records that the pointer is at a particular position.
+ *
+ * \param client  The client.
+ * \param channel The channel ID.
+ * \param x       The X coordinate.
+ * \param y       The Y coordinate.
+ */
 void xzibit_client_move_pointer (XzibitClient *client,
                                  int channel,
                                  int x,
