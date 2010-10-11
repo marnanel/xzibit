1679a6c73911c866def29c0fd897f15aa3c101ea
Thomas Thurman <tthurman@gnome.org>
2010-10-11 18:13:59 -0400

    Adding contact names to the dialogue works

--- src/connector/contact-chooser.c
+++ src/connector/contact-chooser.c
@@ -1,15 +1,45 @@
 #include "contact-chooser.h"
+#include "list-contacts.h"
 
 #define _(x) (x)
 
 typedef struct _ContactContext {
   gboolean closed;
   gboolean seen_all_contacts;
-  GtkTreeModel *model;
+  GtkListStore *model;
   GtkWidget *treeview;
   GtkWidget *label;
   GHashTable *sources;
 } ContactContext;
+
+static void
+add_contact (const gchar *source,
+	     const gchar *target,
+	     gpointer user_data)
+{
+  ContactContext *context = (ContactContext*) user_data;
+  GtkTreeIter iter;
+
+  if (!source && !target)
+    {
+      context->seen_all_contacts = TRUE;
+      gtk_label_set_text (GTK_LABEL (context->label),
+			  _("Please choose a contact to "
+			    "share this window with."));
+      return;
+    }
+
+  g_hash_table_insert (context->sources,
+		       (gpointer) target,
+		       (gpointer) source);
+
+  gtk_list_store_append (context->model,
+			 &iter);
+  gtk_list_store_set (context->model,
+		      &iter,
+		      0, target,
+		      -1);
+}
 
 GtkWidget*
 show_contact_chooser (int window_id,
@@ -30,6 +60,8 @@ show_contact_chooser (int window_id,
     gtk_vbox_new (0, FALSE);
   GtkWidget *treeview;
   GdkGeometry geometry;
+  GtkCellRenderer *renderer;
+  GtkTreeViewColumn *column;
 
   geometry.min_width = 300;
   geometry.min_height = 300;
@@ -43,9 +75,8 @@ show_contact_chooser (int window_id,
   context->seen_all_contacts = FALSE;
   context->label =
     gtk_label_new (_("Please wait; checking contacts."));
-  context->model =
-    GTK_TREE_MODEL (gtk_list_store_new (1,
-					G_TYPE_STRING));
+  context->model = gtk_list_store_new (1,
+				       G_TYPE_STRING);
   context->sources =
     g_hash_table_new_full (g_str_hash,
 			   g_str_equal,
@@ -53,7 +84,15 @@ show_contact_chooser (int window_id,
 			   g_free);
 
   treeview =
-    gtk_tree_view_new_with_model (context->model);
+    gtk_tree_view_new_with_model (GTK_TREE_MODEL (context->model));
+
+  renderer = gtk_cell_renderer_text_new ();
+  column = gtk_tree_view_column_new_with_attributes ("Contact",
+						     renderer,
+						     "text", 0,
+						     NULL);
+  gtk_tree_view_append_column (GTK_TREE_VIEW (treeview),
+			       column);
 
   gtk_container_add (GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(window))),
 		     vbox);
@@ -66,6 +105,10 @@ show_contact_chooser (int window_id,
 		    FALSE, FALSE, 0);
   
   gtk_widget_show_all (GTK_WIDGET (window));
+
+  list_contacts (add_contact,
+		 "x-xzibit",
+		 context);
 
   return window;
 }
