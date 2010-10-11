a90168ffa7c6cf4e594bb66b11469e06cab89a66
Thomas Thurman <tthurman@gnome.org>
2010-10-11 12:05:50 -0400

    params to this callback should be const

--- src/connector/list-contacts.h
+++ src/connector/list-contacts.h
@@ -1,7 +1,7 @@
 #ifndef LIST_CONTACTS_H
 #define LIST_CONTACTS_H 1
 
-typedef void list_contacts_cb(gchar*, gchar*);
+typedef void list_contacts_cb(const gchar*, const gchar*);
 
 void list_contacts (list_contacts_cb *callback,
 		    gchar *wanted_service);
