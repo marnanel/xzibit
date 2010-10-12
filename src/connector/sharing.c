500c877f4a4d6804e5ff346dd0f7436e641d4fde
Thomas Thurman <tthurman@gnome.org>
2010-10-12 15:55:07 -0400

    some basic test code for sharing

--- src/connector/sharing.c
+++ src/connector/sharing.c
@@ -1,5 +1,6 @@
 #include "sharing.h"
 #include <gtk/gtk.h>
+#include <gdk/gdkx.h>
 
 guint
 window_get_sharing (Window window)
@@ -19,6 +20,37 @@ window_set_sharing (Window window,
 
 #ifdef SHARING_TEST
 
+int stage = 0;
+
+static gboolean
+timeout (gpointer user_data)
+{
+  GtkWidget *window =
+    (GtkWidget*) user_data;
+  Window id =
+    GDK_WINDOW_XID (window->window);
+
+  switch (++stage)
+    {
+    case 1:
+    case 3:
+      g_print ("State of window %x is %d\n",
+	       id, window_get_sharing (id));
+      break;
+
+    case 2:
+      g_print ("Sharing the window.\n");
+      window_set_sharing (id, 1,
+			  NULL, NULL);
+      break;
+
+    case 4:
+      return FALSE;
+    }
+
+  return TRUE;
+}
+
 int
 main (int argc, char **argv)
 {
@@ -35,6 +67,10 @@ main (int argc, char **argv)
 		    NULL);
 
   gtk_widget_show_all (window);
+
+  g_timeout_add (1000,
+		 timeout,
+		 window);
 
   gtk_main ();
 }
