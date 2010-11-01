e5e9be198ccc776dff8bd9f34c42ba8924461b16
Thomas Thurman <tthurman@gnome.org>
2010-11-01 09:07:07 -0400

    undecorated window

--- src/floater.c
+++ src/floater.c
@@ -13,6 +13,7 @@ main(int argc, char **argv)
 
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   g_signal_connect (window, "delete_event", G_CALLBACK (gtk_main_quit), NULL);
+  gtk_window_set_decorated (GTK_WINDOW (window), FALSE);
 
   image = gtk_image_new_from_file ("romeo.png");
   gtk_container_add (GTK_CONTAINER (window),
