cdde45c2b916b9e88f067ad14b6126900e0bc0c2
Thomas Thurman <tthurman@gnome.org>
2010-11-01 09:02:50 -0400

    doodle of non-pointer-based cursors

---
+++ src/floater.c
@@ -0,0 +1,19 @@
+/*
+  g_signal_connect (window, "delete_event", G_CALLBACK (gtk_main_quit), NULL);
+*/
+#include <gtk/gtk.h>
+
+int
+main(int argc, char **argv)
+{
+  GtkWidget *window;
+
+  gtk_init (&argc, &argv);
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  g_signal_connect (window, "delete_event", G_CALLBACK (gtk_main_quit), NULL);
+
+  gtk_widget_show_all (window);
+
+  gtk_main ();
+}
